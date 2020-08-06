f3d7de44443042733b397f80e72742d86ac14369
Jinesh-prajapati <prajapatijinesh3@gmail.com>
2020-08-06 14:25:41 +0530

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HomeComponent } from "./home/home.component";
 import { DragdropComponent } from "./dragdrop/dragdrop.component";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Login1Component } from './login1/login1.component';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DragdropComponent],
+  declarations: [AppComponent, HomeComponent, DragdropComponent, LoginComponent, RegisterComponent, Login1Component],
   imports: [
     BrowserModule,
     DragDropModule,
